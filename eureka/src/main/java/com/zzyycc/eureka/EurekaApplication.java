2f69887f3d2a63e5929408d0aadd1816cfe99087
zhuyuechao@aliyun.com <zhuyuchao199682>
2022-02-15 16:53:11 +0800

    new

--- eureka/src/main/java/com/zzyycc/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/zzyycc/eureka/EurekaApplication.java
@@ -3,6 +3,7 @@ package com.zzyycc.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class EurekaApplication {
 
